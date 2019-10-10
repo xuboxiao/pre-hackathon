c1e89e2081b91f1d1d1920d9ea814cb8c570fbef
grb9in <56060083+grb9in@users.noreply.github.com>
2019-10-10 10:09:00 +0800

    Add files via upload

---
+++ DML-2.sql
@@ -0,0 +1,21 @@
+TRUNCATE TABLE PRODUCT;
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration,ESG_Rating,Sharpe_ration) values (1,'Tox free solutions',1,'AA',2.0,'AA',2.0);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (2,'SolarCity Corporation',1,'AAA',3.0);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (3,'US Ecology',1,'AA',2.5);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (4,'Citic Envirotech Ltd',1,'AAA',2.7);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (5,'Watawala Plantation',1,'AAA',3.0);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (6,'Solazyme',1,'AAA',2.8);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (7,'Goodyear Tires and Rubber',0,'CCC',1.0);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (8,'Bridgestone Corporation',0,'B',1.0);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (9,'Parkson Retail Group',0,'BB',2.0);
+
+insert into product (product_id, product_name,unit_daily_credit_award,ESG_Rating,Sharpe_ration) values (10,'Indiabulls Property Investements',0,'BBB',3.0);
+COMMIT;
